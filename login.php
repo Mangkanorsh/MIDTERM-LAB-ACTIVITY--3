a3a5478f0998c547668a7b0c4a3124b01150b26f
Mangkanorsh <95523031+Mangkanorsh@users.noreply.github.com>
2024-04-05 17:06:11 +0800

    Update login.php

--- login.php
+++ login.php
@@ -11,11 +11,11 @@
         <form action="login.php" method="post">
             <div class="form-group">
                 <label class="form-label" for="username">Username:</label>
-                <input type="text" name="username" id="username" required>
+                <input type="text" name="username" id="username" placeholder="Username" required>
             </div>
             <div class="form-group">
                 <label class="form-label" for="password">Password:</label>
-                <input type="password" name="password" id="password" required>
+                <input type="password" name="password" id="password" placeholder="Password" required>
             </div>
             <div class="form-group">
                 <input type="submit" name="accept" value="Accept">
